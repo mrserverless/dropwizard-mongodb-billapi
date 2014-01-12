c68cd54dac5244c262c74dcb096a1ad79ba7f602
Yun Zhi Lin <yun@yunspace.com>
2014-01-13 00:55:39 +1100

    removed unused google api dependency from gradle

--- src/main/java/com/apmasphere/billproto/health/MongoHealthCheck.java
+++ src/main/java/com/apmasphere/billproto/health/MongoHealthCheck.java
@@ -2,22 +2,21 @@ package com.apmasphere.billproto.health;
 
 import com.apmasphere.billproto.db.MongoManaged;
 import com.codahale.metrics.health.HealthCheck;
-import com.mongodb.Mongo;
 
 /**
  * Created by yun on 10/01/2014.
  */
 public class MongoHealthCheck extends HealthCheck {
 
-        private Mongo mongo;
+        private MongoManaged mongo;
 
         public MongoHealthCheck(MongoManaged mongoManaged) {
-            this.mongo = mongoManaged.getMongo();
+            this.mongo = mongoManaged;
         }
 
         @Override
         protected Result check() throws Exception {
-            mongo.getVersion();
+            mongo.getDb().getCollectionNames();
             return Result.healthy();
         }
 
